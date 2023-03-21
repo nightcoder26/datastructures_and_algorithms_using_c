36436afc6fc49863850649d9f56348789694e27b
Bhavitha <77844703+nightcoder26@users.noreply.github.com>
2023-03-21 19:31:07 +0530

    Update TRAVERSING_AN_ARRAY.c

--- ARRAYS/TRAVERSING_AN_ARRAY.c
+++ ARRAYS/TRAVERSING_AN_ARRAY.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
-
-int main() {
-   int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-   int i;
-
-   for(i = 0; i < 10; i++)
-      printf("%d ", array[i]);
-      
-   return 0;
+// A C program to traverse an entire array to print or display n no's
+#include<stdio.h>
+int main(){
+	int i, n;
+	printf("\n Enter the number of elements in the array : ");
+	scanf("%d",&n);
+	int a[n];
+	for(i = 0; i < n;i++){
+		printf("\n Arr[%d] = ",i);
+		scanf("%d",&a[i]);
+	}
+	printf("\nThe array elements are  :\n");
+	for(i = 0; i < n; i++){
+		printf("%d\t",a[i]);
+	}
+	return 0;
+	
 }
